2b40f3f140978cee0a2a5c5de9399b23d36769f5
NicolleDanielaSarmientoSerranoADSO <daniisarmientose26@gmail.com>
2022-09-28 22:15:34 -0500

    Ejercicio3

--- main3.js
+++ main3.js
@@ -1,19 +1,18 @@
 addEventListener("DOMContentLoaded", (e)=>{
 let cont=0
-while(cont<10){
+while (cont<=9){
     let n1=0,n2=0
-    n1=Number(prompt("Ingrese el primer numero: "))
-    n2=Number(prompt("Ingrese el segundo numero:"))
+    n1=Number(prompt(`Ingrese el primer numero:`))
+    n2=Number(prompt(`Ingrese el segundo numero:`))
     let sum=0,rest=0,div=0,mult=0
     sum=(n1+n2)
     rest=(n1-n2)
-    mult=(n1*n2)
     div=(n1/n2)
+    mult=(n1*n2)
+    cont+=1
     document.write(`El total de la suma es de: ${sum}<br>`)
     document.write(`El total de la resta es de: ${rest}<br>`)
     document.write(`El total de la división es de: ${div}<br>`)
     document.write(`El total de la multiplicación es de: ${mult}<br>`)
-    document.write(`_______________________<br>`)
-    cont+=1
 }
 })
